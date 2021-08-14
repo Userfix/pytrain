0378840e85ce8a1280c629f8e74a6cef09c6b596
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-08-14 11:47:26 +0300

    separate a fixture for all tests

--- fixture/session.py
+++ fixture/session.py
@@ -7,15 +7,14 @@ class SessionHelper:
         wd = self.app.wd
         self.app.open_home_page()
         # login
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_id("LoginForm").submit()
-        wd.find_element_by_id("footer").click()
 
     def logout(self):
         wd = self.app.wd
         # logout
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
