db7c0574364f89452761c7057837c8a525bf5087
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-08-15 19:26:28 +0300

    * refactor to separate "groups" classes * add modify title checks for filing forms * move login and logout to fixture

--- fixture/group.py
+++ fixture/group.py
@@ -10,6 +10,10 @@ class GroupHelper:
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
@@ -19,33 +23,42 @@ class GroupHelper:
 
     def changes(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
         # submit group form
         wd.find_element_by_xpath("//input[@type='submit']").click()
         self.return_to_groups_page()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def edit_first_group(self, group):
+        # my modify
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
-        # submit deletion
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
         self.changes(group)
+
+    def modify_first_group(self, new_group_data):
+        # course modify
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_first_group()
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.changes(new_group_data)
 
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
