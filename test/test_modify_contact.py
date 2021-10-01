f1f94e57bc53e7be701408ee0e6536d467d94762
Konstantin Nosarev <46044488+Userfix@users.noreply.github.com>
2021-10-01 20:26:00 +0300

    add contacts verification using db list

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -7,13 +7,15 @@ def test_modify_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="NewFirstName 1"))
     old_contacts = db.get_contact_list()
-    contact = random.choice(old_contacts)
-    contactdata = Contact(firstname="Updated FirstName", middlename="Updated MiddleName", lastname="Updated LastName",
+    index = random.randrange(len(old_contacts))
+    contact = Contact(firstname="Updated FirstName", middlename="Updated MiddleName", lastname="Updated LastName",
                           nickname="UpdNickname", title="ABC", company="Solar", address="Gogolya 2",
                           homephone="84953333333", mobilephone="+7499000000", email1="quest@test.com",
                           bday="11", bmonth="July", byear="1862")
-    app.contact.modify_contact_by_id(contact.id, contactdata)
+    contact.id = old_contacts[index].id
+    app.contact.modify_contact_by_id(contact.id, contact)
     new_contacts = db.get_contact_list()
-    assert len(old_contacts) == len(new_contacts)
+    old_contacts[index] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
